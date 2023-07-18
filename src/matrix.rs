195d58f1849f8eee4ebf4214a459730e0dd67d8f
Neng <neng@ds.dfci.harvard.edu>
2023-07-18 17:23:44 -0400

    ColumnBaseCount

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,4 +1,7 @@
+use std::process;
+use std::cmp::max;
 use std::collections::HashMap;
+// use rust_htslib::htslib::exit;
 use crate::bam_reader::Region;
 
 #[derive(Clone)]
@@ -98,4 +101,132 @@ pub struct ColumnBaseCount {
     n_blank: u16,
     max_count: u16,
     ref_base: u8,
+}
+
+impl ColumnBaseCount {
+    pub fn new() -> ColumnBaseCount {
+        ColumnBaseCount {
+            n_a: 0,
+            n_c: 0,
+            n_g: 0,
+            n_t: 0,
+            n_n: 0,
+            n_dash: 0,
+            n_blank: 0,
+            max_count: 0,
+            ref_base: 0,
+        }
+    }
+
+    pub fn new_from_column(matrix_column: &Vec<u8>, ref_base: u8) -> ColumnBaseCount {
+        let mut cbc = ColumnBaseCount {
+            n_a: 0,
+            n_c: 0,
+            n_g: 0,
+            n_t: 0,
+            n_n: 0,
+            n_dash: 0,
+            n_blank: 0,
+            max_count: 0,
+            ref_base: ref_base,
+        };
+
+        for b in matrix_column.iter() {
+            match b {
+                b'A' => cbc.n_a += 1,
+                b'a' => cbc.n_a += 1,
+                b'C' => cbc.n_c += 1,
+                b'c' => cbc.n_c += 1,
+                b'G' => cbc.n_g += 1,
+                b'g' => cbc.n_g += 1,
+                b'T' => cbc.n_t += 1,
+                b't' => cbc.n_t += 1,
+                b'N' => cbc.n_n += 1,
+                b'n' => cbc.n_n += 1,
+                b'-' => cbc.n_dash += 1,
+                b' ' => cbc.n_blank += 1,
+                _ => (),
+            }
+        }
+
+        // TODO: reduce the effect of N when calculate score.
+        cbc.n_n = 1;
+
+        // max_count does not consider padding (blank).
+        cbc.max_count = max(max(max(max(max(cbc.n_a, cbc.n_c), cbc.n_g), cbc.n_t), cbc.n_n), cbc.n_dash);
+        return cbc;
+    }
+
+    pub fn get_major_base(&self) -> u8 {
+        if self.n_a == self.max_count {
+            return b'A';
+        } else if self.n_c == self.max_count {
+            return b'C';
+        } else if self.n_g == self.max_count {
+            return b'G';
+        } else if self.n_t == self.max_count {
+            return b'T';
+        } else if self.n_n == self.max_count {
+            return b'N';
+        } else if self.n_dash == self.max_count {
+            return b'-';
+        } else if self.n_blank == self.max_count {
+            return b' ';
+        } else {
+            println!("Error: get_major_base() failed, exit.");
+            process::exit(1);
+            return 0;
+        }
+    }
+
+    pub fn get_ref_base(&self) -> u8 {
+        self.ref_base
+    }
+
+    pub fn get_score1(&self, x: &u8) -> i32 {
+        match x {
+            b'A' => 1 - (self.n_a == self.max_count) as i32,
+            b'a' => 1 - (self.n_a == self.max_count) as i32,
+            b'C' => 1 - (self.n_c == self.max_count) as i32,
+            b'c' => 1 - (self.n_c == self.max_count) as i32,
+            b'G' => 1 - (self.n_g == self.max_count) as i32,
+            b'g' => 1 - (self.n_g == self.max_count) as i32,
+            b'T' => 1 - (self.n_t == self.max_count) as i32,
+            b't' => 1 - (self.n_t == self.max_count) as i32,
+            b'N' => 1 - (self.n_n == self.max_count) as i32,
+            b'n' => 1 - (self.n_n == self.max_count) as i32,
+            b'-' => 1 - (self.n_dash == self.max_count) as i32,
+            _ => {
+                println!("Error: get_score1() failed, exit.");
+                process::exit(1);
+                return 0;
+            }
+        }
+    }
+
+    pub fn get_score2(&self, x: &u8) -> f64 {
+        let s: f64 = self.n_a + self.n_c + self.n_g + self.n_t + self.n_dash as f64;
+        if s == 0.0 {
+            return 0.0;
+        } else {
+            match x {
+                b'A' => (s - self.n_a as f64) / s,
+                b'a' => (s - self.n_a as f64) / s,
+                b'C' => (s - self.n_c as f64) / s,
+                b'c' => (s - self.n_c as f64) / s,
+                b'G' => (s - self.n_g as f64) / s,
+                b'g' => (s - self.n_g as f64) / s,
+                b'T' => (s - self.n_t as f64) / s,
+                b't' => (s - self.n_t as f64) / s,
+                b'N' => (s - self.n_n as f64) / s,
+                b'n' => (s - self.n_n as f64) / s,
+                b'-' => (s - self.n_dash as f64) / s,
+                _ => {
+                    println!("Error: get_score2() failed, exit.");
+                    process::exit(1);
+                    return 0.0;
+                }
+            }
+        }
+    }
 }
