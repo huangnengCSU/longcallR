2c4c9316aa4889d6de70263654cefd44bfd77f10
Neng <neng@ds.dfci.harvard.edu>
2023-09-01 13:25:37 -0400

    remove comments

--- src/util.rs
+++ src/util.rs
@@ -42,7 +42,6 @@ use crate::base_matrix::get_chrom_coverage_intervals;
 }*/
 
 pub fn multithread_produce(bam_file: String, thread_size: usize, tx_low: mpsc::Sender<(String, i64, i64)>, tx_high: mpsc::Sender<(String, i64, i64)>) {
-    // let mut produce_v = vec![];
     let pool = ThreadPool::new(thread_size);
     let bam = bam::IndexedReader::from_path(bam_file.clone()).unwrap();
     let bam_header = bam.header().clone();
@@ -50,19 +49,12 @@ pub fn multithread_produce(bam_file: String, thread_size: usize, tx_low: mpsc::S
     for ctg in bam_header.target_names() {
         contig_names.push_back(std::str::from_utf8(ctg).unwrap().to_string().clone());
     }
-    // let njobs = contig_names.len();
-    //
-    // let shared_ctgnames = Arc::new(Mutex::new(contig_names));
-
     while !contig_names.is_empty() {
         let ctg = contig_names.pop_front().unwrap();
         let tx_l = tx_low.clone();
         let tx_h = tx_high.clone();
         let bam_file_clone = bam_file.clone();
         pool.execute(move || {
-            // let mut contig_name = shared_ctgnames.lock().unwrap();
-            // let ctg = contig_name.pop_front().unwrap();
-            println!("{}", ctg);
             let (normal_depth_regions, high_depth_regions) = get_chrom_coverage_intervals(bam_file_clone.clone(), ctg.as_str(), 1000);
             for region in normal_depth_regions {
                 tx_l.send(region).unwrap();
@@ -73,26 +65,4 @@ pub fn multithread_produce(bam_file: String, thread_size: usize, tx_low: mpsc::S
         });
     }
     pool.join();
-
-    // for i in 0..thread_size {
-    //     let bam_file_clone = bam_file.clone();
-    //     let shared_ctgnames = Arc::clone(&shared_ctgnames);
-    //     let tx_l = tx_low.clone();
-    //     let tx_h = tx_high.clone();
-    //     let handle = thread::spawn(move || {
-    //         let mut contig_name = shared_ctgnames.lock().unwrap();
-    //         while !contig_name.is_empty() {
-    //             let ctg = contig_name.pop_front().unwrap();
-    //             println!("thread {}: {}", i, ctg);
-    //             let (normal_depth_regions, high_depth_regions) = get_chrom_coverage_intervals(bam_file_clone.clone(), ctg.as_str(), 1000);
-    //             for region in normal_depth_regions {
-    //                 tx_l.send(region).unwrap();
-    //             }
-    //             for region in high_depth_regions {
-    //                 tx_h.send(region).unwrap();
-    //             }
-    //         }
-    //     });
-    //     produce_v.push(handle);
-    // }
 }
