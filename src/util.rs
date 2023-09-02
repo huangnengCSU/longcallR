676a9ae29cd9325ac12e7f0a734b62df0c7d94a0
huangneng <neng@ds.dfci.harvard.edu>
2023-09-02 16:18:40 -0400

    add processing for high_depth_regions

--- src/util.rs
+++ src/util.rs
@@ -37,6 +37,8 @@ pub fn multithread_work(bam_file: String, ref_file: String, out_bam: String, thr
     let pool = ThreadPool::new(thread_size);
     let bam_records_queue: Arc<Mutex<VecDeque<bam::Record>>> = Arc::new(Mutex::new(VecDeque::new()));
     let ref_seqs = load_reference(ref_file.to_string());
+
+    // multiplethreads for low coverage regions
     for normal_depth_region in rx_low {
         let bam_file_clone = bam_file.clone();
         let out_bam_clone = out_bam.clone();
@@ -67,9 +69,47 @@ pub fn multithread_work(bam_file: String, ref_file: String, out_bam: String, thr
                 queue.push_back(record);
             }
             if queue.len() > 100000 {
-                println!("{} records in queue", queue.len());
+                println!("low coverage {} records in queue", queue.len());
                 queue.clear();
             }
         });
     }
+    bam_records_queue.lock().unwrap().clear();
+
+    for high_depth_region in rx_high {
+        let bam_file_clone = bam_file.clone();
+        let out_bam_clone = out_bam.clone();
+        let ref_seqs_clone = ref_seqs.clone();
+        let bam_records_queue = Arc::clone(&bam_records_queue);
+        pool.execute(move || {
+            let mut base_matrix = BaseMatrix::new();
+            base_matrix.load_data_without_extension(bam_file_clone.clone().to_string(), high_depth_region);
+            base_matrix.load_ref_data(ref_seqs_clone.clone());
+            base_matrix.expand_insertion();
+            let (forward_donor_penalty,
+                forward_acceptor_penalty,
+                reverse_donor_penalty,
+                reverse_acceptor_penalty) = get_donor_acceptor_penalty(&base_matrix.expanded_matrix, 30.0);
+            let mut best_reduced_expanded_matrix: HashMap<String, Vec<u8>> = HashMap::new();
+            let mut best_column_indexes: Vec<usize> = Vec::new();
+            profile_realign(&base_matrix.expanded_matrix,
+                            &forward_donor_penalty,
+                            &forward_acceptor_penalty,
+                            &reverse_donor_penalty,
+                            &reverse_acceptor_penalty,
+                            &mut best_reduced_expanded_matrix,
+                            &mut best_column_indexes);
+            update_expanded_matrix_from_realign(&mut base_matrix.expanded_matrix, &best_reduced_expanded_matrix, &best_column_indexes);
+            update_bam_records_from_realign(&mut base_matrix.expanded_matrix, &mut base_matrix.bam_records, base_matrix.start_position, base_matrix.end_position);
+            let mut queue = bam_records_queue.lock().unwrap();
+            for (_, record) in base_matrix.bam_records {
+                queue.push_back(record);
+            }
+            if queue.len() > 100000 {
+                println!("high coverage {} records in queue", queue.len());
+                queue.clear();
+            }
+        });
+    }
+    bam_records_queue.lock().unwrap().clear();
 }
