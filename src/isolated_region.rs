9f202fa6438010f0d1cb011c9bc7ce0a2b3eceb5
Neng <neng@ds.dfci.harvard.edu>
2023-09-19 14:58:00 -0400

    fix error in find_isolated_regions

--- src/isolated_region.rs
+++ src/isolated_region.rs
@@ -1,5 +1,5 @@
-use rust_htslib::{bam, bam::Read};
 use crate::bam_reader::Region;
+use rust_htslib::{bam, bam::Read};
 
 pub fn find_isolated_regions(bam_path: &str, min_depth: u32) -> Vec<Region> {
     let mut bam: bam::IndexedReader = bam::IndexedReader::from_path(bam_path).unwrap();
@@ -16,39 +16,48 @@ pub fn find_isolated_regions(bam_path: &str, min_depth: u32) -> Vec<Region> {
         let tid = pileup.tid();
         let depth = pileup.depth();
         // println!("{} {} {}", pos, tid, depth);
-        if pre_tid != 0 && tid != pre_tid {
-            if start_pos != 0 && end_pos != 0 {
-                let region = Region {
-                    chr: std::str::from_utf8(&header.tid2name(pre_tid as u32)).unwrap().to_string(),
-                    start: start_pos,
-                    end: end_pos + 1,
-                };
-                if max_depth >= min_depth {
-                    isolated_regions.push(region);
-                }
-                start_pos = 0;
-                end_pos = 0;
-                pre_tid = 0;
-            }
-        }
 
-        if pre_tid == 0 && start_pos == 0 && end_pos == 0 {
+        if pre_tid == 0 {
             pre_tid = tid;
             start_pos = pos;
             end_pos = pos;
             max_depth = depth;
-        } else if pos != end_pos + 1 && pre_tid != 0 && start_pos != 0 && end_pos != 0 {
-            let region = Region {
-                chr: std::str::from_utf8(&header.tid2name(pre_tid as u32)).unwrap().to_string(),
-                start: start_pos,
-                end: end_pos + 1,
-            };
-            if max_depth >= min_depth {
+            continue;
+        }
+
+        if tid != pre_tid && pre_tid != 0 {
+            if end_pos > start_pos && max_depth >= min_depth {
+                let region = Region {
+                    chr: std::str::from_utf8(&header.tid2name(pre_tid as u32))
+                        .unwrap()
+                        .to_string(),
+                    start: start_pos + 1,
+                    end: end_pos + 1,
+                };
                 isolated_regions.push(region);
             }
-            start_pos = 0;
-            end_pos = 0;
-            pre_tid = 0;
+            start_pos = pos;
+            end_pos = pos;
+            pre_tid = tid;
+            max_depth = depth;
+            continue;
+        }
+
+        if pos != end_pos + 1 {
+            if end_pos > start_pos && max_depth >= min_depth {
+                let region = Region {
+                    chr: std::str::from_utf8(&header.tid2name(pre_tid as u32))
+                        .unwrap()
+                        .to_string(),
+                    start: start_pos + 1,
+                    end: end_pos + 1,
+                };
+                isolated_regions.push(region);
+            }
+            start_pos = pos;
+            end_pos = pos;
+            pre_tid = tid;
+            max_depth = depth;
         } else {
             end_pos = pos;
             if depth > max_depth {
@@ -56,5 +65,17 @@ pub fn find_isolated_regions(bam_path: &str, min_depth: u32) -> Vec<Region> {
             }
         }
     }
+
+    if end_pos > start_pos && max_depth >= min_depth {
+        let region = Region {
+            chr: std::str::from_utf8(&header.tid2name(pre_tid as u32))
+                .unwrap()
+                .to_string(),
+            start: start_pos + 1,
+            end: end_pos + 1,
+        };
+        isolated_regions.push(region);
+    }
+
     return isolated_regions;
 }
