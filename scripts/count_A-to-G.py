d7d12bf594bc715365edfed6385d669c69cee9bd
neng huang <csuhuangneng@gmail.com>
2024-02-20 10:11:56 -0500

    r1.1

---
+++ scripts/count_A-to-G.py
@@ -0,0 +1,47 @@
+import sys
+
+hap_vcf = sys.argv[1]  ## hap.py output vcf file (decompressed)
+
+fp_out = open("fp_sites.csv", 'w')
+fn_out = open("fn_sites.csv", 'w')
+non_A_to_G_tp, non_A_to_G_fp, non_A_to_G_fn = 0, 0, 0
+with open(hap_vcf, 'r') as f:
+    tp_cnt, fp_cnt, fn_cnt = 0, 0, 0
+    for line_str in f:
+        if line_str.startswith('#'):
+            continue
+        else:
+            line = line_str.strip().split('\t')
+            ref = line[3]
+            alt = line[4]
+            truth = line[9]
+            query = line[10]
+            if (ref == 'A' and alt == 'G') or (ref == 'T' and alt == 'C'):
+                if "TP" in truth and "SNP" in truth and "TP" in query:
+                    tp_cnt += 1
+                else:
+                    ## An error genotype will both increase a FP and a FN
+                    if "FN" in truth and "SNP" in truth:
+                        fn_cnt += 1
+                    if "FP" in query and "SNP" in query:
+                        fp_cnt += 1
+            else:
+                ## non RNA-editing sites
+                if "TP" in truth and "SNP" in truth and "TP" in query:
+                    non_A_to_G_tp += 1
+                else:
+                    ## An error genotype will both increase a FP and a FN
+                    if "FN" in truth and "SNP" in truth:
+                        non_A_to_G_fn += 1
+                        fn_out.write(line_str)
+                    if "FP" in query and "SNP" in query:
+                        non_A_to_G_fp += 1
+                        fp_out.write(line_str)
+print(f"A-to-G TP: {tp_cnt}, A-to-G FN: {fn_cnt}, A-to-G FP: {fp_cnt}")
+print(f"Non A-to-G TP = {non_A_to_G_tp},\nNon A-to-G FN = {non_A_to_G_fn},\nNon A-to-G FP = {non_A_to_G_fp}")
+P = non_A_to_G_tp / (non_A_to_G_tp + non_A_to_G_fp)
+R = non_A_to_G_tp / (non_A_to_G_tp + non_A_to_G_fn)
+F1 = 2 * P * R / (P + R)
+print(f"Recall = {R},\nPrecision = {P},\nF1 = {F1}")
+fp_out.close()
+fn_out.close()
---
+++ scripts/plot_allele_freq_distribution.py
@@ -0,0 +1,60 @@
+import matplotlib
+matplotlib.use('AGG')
+import matplotlib.pyplot as plt
+import sys
+
+def load_dna_hete_snps(dna_snp_truth):
+    dna_hete_snps = set()
+    with open(dna_snp_truth, 'r') as f:
+        for line in f:
+            if line.startswith("#"):
+                continue
+            fields = line.strip().split("\t")
+            chrom = fields[0]
+            pos = int(fields[1])
+            gt = fields[-1].split(":")[0]
+            if gt== "0/1" or gt == "1/0":
+                dna_hete_snps.add((chrom, pos))
+    return dna_hete_snps
+
+def load_rna_allele_freq(rna_allele_freq, dna_hete_snps, min_allele_cnt):
+    rna_allele_freq_dict = {}
+    with open(rna_allele_freq, 'r') as f:
+        for line in f:
+            fields = line.strip().split('\t')
+            chrom = fields[0]
+            pos = int(fields[1])
+            if (chrom, pos) not in dna_hete_snps:
+                continue
+            ref = fields[2]
+            alt = fields[3].split(",")
+            alt_cnt = fields[4].split(":")[1].split(",")
+            total_allele_cnt = sum([int(x) for x in alt_cnt])
+            if total_allele_cnt < min_allele_cnt:
+                continue
+            if len(alt)==1 and alt[0] in ["A", "C", "G", "T"]:
+                rna_allele_freq_dict[(chrom, pos)] = 1.0
+            elif len(alt)==2 and alt[0] in ["A", "C", "G", "T"] and alt[1] in ["A", "C", "G", "T"]:
+                if alt[0] == ref:
+                    rna_allele_freq_dict[(chrom, pos)] = int(alt_cnt[1])/(int(alt_cnt[0])+int(alt_cnt[1]))
+                elif alt[1] == ref:
+                    rna_allele_freq_dict[(chrom, pos)] = int(alt_cnt[0])/(int(alt_cnt[0])+int(alt_cnt[1]))
+                else:
+                    ## 1/2
+                    continue
+    return rna_allele_freq_dict
+
+def plot_allele_freq(rna_allele_freq_dict):
+    allele_freq = list(rna_allele_freq_dict.values())
+    plt.hist(allele_freq, bins=100)
+    plt.xlabel('RNA allele frequency')
+    plt.ylabel('Number of SNPs')
+    plt.title('RNA allele frequency distribution')
+    plt.savefig('allele_freq.png')
+
+dna_snp_truth = sys.argv[1] ## DNA SNP truth set
+rna_allele_freq = sys.argv[2]   ## htsbox mpileup output (htsbox pileup -f -v)
+min_allele_cnt = int(sys.argv[3])   ## ignore sites with allele count less than this value
+dna_hete_snps = load_dna_hete_snps(dna_snp_truth)
+rna_allele_freq_dict = load_rna_allele_freq(rna_allele_freq, dna_hete_snps, min_allele_cnt)
+plot_allele_freq(rna_allele_freq_dict)
