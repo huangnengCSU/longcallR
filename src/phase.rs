61ccc81db86a143543e34cc364c777d690d0c1f7
Neng Huang <neng@ds.dfci.harvard.edu>
2024-04-08 10:37:01 -0400

    calculate_prob_somatic

--- src/phase.rs
+++ src/phase.rs
@@ -7,6 +7,7 @@ use std::io::Write;
 use std::sync::Mutex;
 
 use bio::bio_types::strand::ReqStrand::Forward;
+use mathru::algebra::abstr::cast::ToPrimitive;
 use petgraph::algo::kosaraju_scc;
 use petgraph::prelude::*;
 use probability::distribution::Distribution;
@@ -24,6 +25,14 @@ pub struct HapQuals {
     hap1_alt_baseqs: Vec<u8>,
     hap2_ref_baseqs: Vec<u8>,
     hap2_alt_baseqs: Vec<u8>,
+}
+
+#[derive(Debug, Clone, Default)]
+pub struct AlleleClass {
+    pub allcls: u8,
+    // 0: ref, 1: hete, 2: somatic
+    pub prob: f64,
+    // probability of allele class
 }
 
 #[derive(Debug, Clone, Default)]
@@ -1528,7 +1537,7 @@ impl SNPFrag {
         }
     }
 
-    pub fn get_somatic_hapquals(&mut self, bam_path: &str, region: &Region, phased_fragments: &HashMap<String, i32>) {
+    pub fn get_somatic_haplotype_baseqs(&mut self, bam_path: &str, region: &Region, phased_fragments: &HashMap<String, i32>) {
         let mut bam_reader: bam::IndexedReader = bam::IndexedReader::from_path(bam_path).unwrap();
         bam_reader.fetch((region.chr.as_str(), region.start, region.end)).unwrap();
         let mut record = Record::new();
@@ -1653,7 +1662,12 @@ impl SNPFrag {
             }
         }
         for idx in self.somatic_snps.iter() {
-            println!("somatic snp:{:?}\n, hap1_ref_baseqs:{:?}\n, hap1_alt_baseqs:{:?}\n, hap2_ref_baseqs:{:?}\n, hap2_alt_baseqs:{:?}\n", self.candidate_snps[*idx].pos, self.candidate_snps[*idx].hap_quals.hap1_ref_baseqs, self.candidate_snps[*idx].hap_quals.hap1_alt_baseqs, self.candidate_snps[*idx].hap_quals.hap2_ref_baseqs, self.candidate_snps[*idx].hap_quals.hap2_alt_baseqs);
+            println!("somatic snp:{:?}\n, hap1_ref_baseqs:{:?}\n, hap1_alt_baseqs:{:?}\n, hap2_ref_baseqs:{:?}\n, hap2_alt_baseqs:{:?}\n",
+                     self.candidate_snps[*idx].pos,
+                     self.candidate_snps[*idx].hap_quals.hap1_ref_baseqs,
+                     self.candidate_snps[*idx].hap_quals.hap1_alt_baseqs,
+                     self.candidate_snps[*idx].hap_quals.hap2_ref_baseqs,
+                     self.candidate_snps[*idx].hap_quals.hap2_alt_baseqs);
         }
     }
 
@@ -3058,14 +3072,25 @@ impl SNPFrag {
         // detect confident somatic mutation with phasing result of high allele fraction het snps.
         // 1. assign phased result for candidate somatic sites.
         let mut phased_fragments: HashMap<String, i32> = HashMap::new(); // read id, assignment
-        for i in 0..self.fragments.len() {
-            let frag = &self.fragments[i];
+        for k in 0..self.fragments.len() {
+            let frag = &self.fragments[k];
             if frag.assignment == 1 || frag.assignment == 2 {
                 phased_fragments.insert(frag.read_id.clone(), frag.assignment);
             }
         }
-        self.get_somatic_hapquals(bam_path, region, &phased_fragments);
+        self.get_somatic_haplotype_baseqs(bam_path, region, &phased_fragments);
         // 2. find candidates meet the criteria of somatic mutation. haplotype-specific
+        for i in 0..self.somatic_snps.len() {
+            let som_cand = &self.candidate_snps[self.somatic_snps[i]];
+            let (hap1_allele_class, hap2_allele_class) = calculate_prob_somatic(&som_cand.hap_quals.hap1_ref_baseqs, &som_cand.hap_quals.hap1_alt_baseqs, &som_cand.hap_quals.hap2_ref_baseqs, &som_cand.hap_quals.hap2_alt_baseqs, 0.75);
+            if hap1_allele_class.allcls == 0 && hap2_allele_class.allcls == 2 {
+                let somatic_score = -10.0_f64 * (1.0 - hap2_allele_class.prob).log10();
+                println!("somatic snp:{}, score: {}", som_cand.pos, somatic_score);
+            } else if hap1_allele_class.allcls == 2 && hap2_allele_class.allcls == 0 {
+                let somatic_score = -10.0_f64 * (1.0 - hap1_allele_class.prob).log10();
+                println!("somatic snp:{}, score: {}", som_cand.pos, somatic_score);
+            }
+        }
     }
 
 
@@ -4541,6 +4566,84 @@ impl SNPFrag {
         }
         return records;
     }
+}
+
+
+fn calculate_prob_somatic(hap1_ref_baseqs: &Vec<u8>, hap1_alt_baseqs: &Vec<u8>, hap2_ref_baseqs: &Vec<u8>, hap2_alt_baseqs: &Vec<u8>, purity: f64) -> (AlleleClass, AlleleClass) {
+    let mut hap1_allele_class;
+    let mut hap2_allele_class;
+    let som_rate = 5.0 / 1000000.0;    // each haplotype
+    let het_rate = 1.0 / 2000.0;    // each haplotype
+    let ref_rate = 1.0 - het_rate - som_rate;
+
+    // for Hap1
+    let mut prob_read_ref = 1.0;
+    let mut prob_read_het = 1.0;
+    let mut prob_read_som = 1.0;
+
+    // P(read|ref), P(read|het), P(read|som)
+    for q in hap1_ref_baseqs.iter() {
+        let epsilon = 10.0_f64.powf(-(q.to_f64() / 10.0));  // error rate
+        prob_read_ref *= 1.0 - epsilon; // ref->ref
+        prob_read_het *= epsilon;   // alt->ref
+        prob_read_som *= purity * epsilon + (1.0 - purity) * (1.0 - epsilon);   // purity: alt->ref, 1.0-purity: ref->ref
+    }
+    for q in hap1_alt_baseqs.iter() {
+        let epsilon = 10.0_f64.powf(-(q.to_f64() / 10.0));
+        prob_read_ref *= epsilon;   // ref->alt
+        prob_read_het *= 1.0 - epsilon; // alt->alt
+        prob_read_som *= purity * (1.0 - epsilon) + (1.0 - purity) * epsilon; // purity: alt->alt, 1.0-purity: ref->alt
+    }
+
+    // prob * prior
+    let prob_read_ref_with_prior = prob_read_ref * ref_rate;
+    let prob_read_het_with_prior = prob_read_het * het_rate;
+    let prob_read_som_with_prior = prob_read_som * som_rate;
+    let hap1_prob_ref = prob_read_ref_with_prior / (prob_read_ref_with_prior + prob_read_het_with_prior + prob_read_som_with_prior);
+    let hap1_prob_het = prob_read_het_with_prior / (prob_read_ref_with_prior + prob_read_het_with_prior + prob_read_som_with_prior);
+    let hap1_prob_som = prob_read_som_with_prior / (prob_read_ref_with_prior + prob_read_het_with_prior + prob_read_som_with_prior);
+    if hap1_prob_ref > hap1_prob_het && hap1_prob_ref > hap1_prob_som {
+        hap1_allele_class = AlleleClass { allcls: 0, prob: hap1_prob_ref };
+    } else if hap1_prob_het > hap1_prob_ref && hap1_prob_het > hap1_prob_som {
+        hap1_allele_class = AlleleClass { allcls: 1, prob: hap1_prob_het };
+    } else {
+        hap1_allele_class = AlleleClass { allcls: 2, prob: hap1_prob_som };
+    }
+
+    // for Hap2
+    let mut prob_read_ref = 1.0;
+    let mut prob_read_het = 1.0;
+    let mut prob_read_som = 1.0;
+
+    // P(read|ref), P(read|het), P(read|som)
+    for q in hap2_ref_baseqs.iter() {
+        let epsilon = 10.0_f64.powf(-(q.to_f64() / 10.0));  // error rate
+        prob_read_ref *= 1.0 - epsilon; // ref->ref
+        prob_read_het *= epsilon;   // alt->ref
+        prob_read_som *= purity * epsilon + (1.0 - purity) * (1.0 - epsilon);   // purity: alt->ref, 1.0-purity: ref->ref
+    }
+    for q in hap2_alt_baseqs.iter() {
+        let epsilon = 10.0_f64.powf(-(q.to_f64() / 10.0));
+        prob_read_ref *= epsilon;   // ref->alt
+        prob_read_het *= 1.0 - epsilon; // alt->alt
+        prob_read_som *= purity * (1.0 - epsilon) + (1.0 - purity) * epsilon; // purity: alt->alt, 1.0-purity: ref->alt
+    }
+
+    // prob * prior
+    let prob_read_ref_with_prior = prob_read_ref * ref_rate;
+    let prob_read_het_with_prior = prob_read_het * het_rate;
+    let prob_read_som_with_prior = prob_read_som * som_rate;
+    let hap2_prob_ref = prob_read_ref_with_prior / (prob_read_ref_with_prior + prob_read_het_with_prior + prob_read_som_with_prior);
+    let hap2_prob_het = prob_read_het_with_prior / (prob_read_ref_with_prior + prob_read_het_with_prior + prob_read_som_with_prior);
+    let hap2_prob_som = prob_read_som_with_prior / (prob_read_ref_with_prior + prob_read_het_with_prior + prob_read_som_with_prior);
+    if hap2_prob_ref > hap2_prob_het && hap2_prob_ref > hap2_prob_som {
+        hap2_allele_class = AlleleClass { allcls: 0, prob: hap2_prob_ref };
+    } else if hap2_prob_het > hap2_prob_ref && hap2_prob_het > hap2_prob_som {
+        hap2_allele_class = AlleleClass { allcls: 1, prob: hap2_prob_het };
+    } else {
+        hap2_allele_class = AlleleClass { allcls: 2, prob: hap2_prob_som };
+    }
+    return (hap1_allele_class, hap2_allele_class);
 }
 
 fn exon_cluster(
