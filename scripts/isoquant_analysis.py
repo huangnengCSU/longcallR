a688263a0c52dc60d20ed6b9ad2b1c5c4e6780c5
Neng Huang <neng@ds.dfci.harvard.edu>
2024-09-03 13:06:53 -0400

    add haplotype tpm

--- scripts/isoquant_analysis.py
+++ scripts/isoquant_analysis.py
@@ -1,8 +1,8 @@
 import argparse
-import os.path
 
 import pysam
 from scipy.stats import binomtest
+from collections import defaultdict
 
 
 def parse_isoquant_read_assignment(assignment_file, assignment_type={"unique", "unique_minor_difference"},
@@ -25,6 +25,17 @@ def parse_isoquant_read_assignment(assignment_file, assignment_type={"unique", "
                 except KeyError:
                     continue
     return records
+
+
+def parse_isoquant_tpm(tpm_file):
+    tpm_dict = {}
+    with open(tpm_file, 'r') as f:
+        next(f)
+        for line in f:
+            parts = line.strip().split("\t")
+            feature_id, tpm = parts[0], float(parts[1])
+            tpm_dict[feature_id] = tpm
+    return tpm_dict
 
 
 def get_gene_regions(annotation_file):
@@ -126,13 +137,58 @@ def analyze_haplotype_isoform(assignment_file, annotation_file, bam_file, out_fi
                                 f"{gene_id}\t{isoform_id}\t{gene_region['chr']}\t{gene_region['start']}\t{gene_region['end']}\t{ps}\t{hp_counts[0]}\t{hp_counts[1]}\t{p}\n")
 
 
-if __name__ == "__main__":
+def analyze_tpm(assignment_file, annotation_file, bam_file, assignment_type, classification_type, isoquant_gene_tpm,
+                isoquant_transcript_tpm, output_gene_tpm, output_transcript_tpm):
+    isoquant_read_assignment = parse_isoquant_read_assignment(assignment_file, assignment_type, classification_type)
+    gene_regions = get_gene_regions(annotation_file)
+    gene_tpm = parse_isoquant_tpm(isoquant_gene_tpm)
+    transcript_tpm = parse_isoquant_tpm(isoquant_transcript_tpm)
+    gene_tpm_writer = open(output_gene_tpm, "w")
+    gene_tpm_writer.write("Gene\tTPM\tHap1_TPM\tHap2_TPM\n")
+    transcript_tpm_writer = open(output_transcript_tpm, "w")
+    transcript_tpm_writer.write("Isoform\tTPM\tHap1_TPM\tHap2_TPM\n")
+    for gene_id, isoform_records in isoquant_read_assignment.items():
+        gene_region = gene_regions[gene_id]
+        reads_tag = get_reads_tag(bam_file, gene_region["chr"], gene_region["start"], gene_region["end"])
+        gene_dict = defaultdict(int)
+        for isoform_id, read_names in isoform_records.items():
+            ps_dict = defaultdict(int)
+            for rname in read_names:
+                if rname in reads_tag:
+                    read_info = reads_tag[rname]
+                    if read_info['HP']:
+                        ps_dict[read_info['HP']] += 1
+                        gene_dict[read_info['HP']] += 1
+            h1_cnt = ps_dict[1]
+            h2_cnt = ps_dict[2]
+            tpm = transcript_tpm.get(isoform_id, 0)
+            if h1_cnt + h2_cnt == 0:
+                h1_tpm, h2_tpm = 0, 0
+            else:
+                h1_tpm = h1_cnt / (h1_cnt + h2_cnt) * tpm
+                h2_tpm = h2_cnt / (h1_cnt + h2_cnt) * tpm
+            transcript_tpm_writer.write(f"{isoform_id}\t{tpm}\t{h1_tpm}\t{h2_tpm}\n")
+        h1_cnt = gene_dict[1]
+        h2_cnt = gene_dict[2]
+        tpm = gene_tpm.get(gene_id, 0)
+        if h1_cnt + h2_cnt == 0:
+            h1_tpm, h2_tpm = 0, 0
+        else:
+            h1_tpm = h1_cnt / (h1_cnt + h2_cnt) * tpm
+            h2_tpm = h2_cnt / (h1_cnt + h2_cnt) * tpm
+        gene_tpm_writer.write(f"{gene_id}\t{tpm}\t{h1_tpm}\t{h2_tpm}\n")
+    gene_tpm_writer.close()
+    transcript_tpm_writer.close()
 
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--assignment", required=True, help="Isoquant read assignment file")
+    parser.add_argument("-g", "--gene_tpm", required=True, help="Isoquant gene TPM file")
+    parser.add_argument("-t", "--transcript_tpm", required=True, help="Isoquant transcript TPM file")
     parser.add_argument("-a", "--annotation", required=True, help="Annotation file")
     parser.add_argument("-b", "--bam", required=True, help="BAM file")
-    parser.add_argument("-o", "--output", required=True, help="Output tsv file to report ASE events")
+    parser.add_argument("-o", "--output", required=True, help="prefix of output file")
     parser.add_argument('--assignment_type', type=str, nargs='+', default=["unique", "unique_minor_difference"],
                         help='Assignment types to include. Default is ["unique", "unique_minor_difference"].')
     parser.add_argument('--classification_type', type=str, nargs='+',
@@ -144,9 +200,12 @@ if __name__ == "__main__":
                         help="P-value threshold for Binomial test (default: 0.05)")
 
     args = parser.parse_args()
-    if os.path.exists(args.output):
-        raise FileExistsError(f"Output file {args.output} already exists")
+
     assignment_type = set(args.assignment_type)
     classification_type = set(args.classification_type)
-    analyze_haplotype_isoform(args.assignment, args.annotation, args.bam, args.output, assignment_type,
+    analyze_haplotype_isoform(args.assignment, args.annotation, args.bam, args.output + ".ase.tsv", assignment_type,
                               classification_type, args.min_support, args.p_value)
+    analyze_tpm(args.assignment, args.annotation, args.bam, assignment_type, classification_type,
+                args.gene_tpm, args.transcript_tpm,
+                args.output + ".haplotype_gene_tpm.tsv",
+                args.output + ".haplotype_transcript_tpm.tsv")
